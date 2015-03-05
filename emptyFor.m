6f181ff28c43fd7202d1c5a4195a75fd658a29f8
Jon Boley <jdb@jboley.com>
2015-03-05 13:44:37 -0600

    feat(emptyFor): new script to run empty for loops

---
+++ emptyFor.m
@@ -0,0 +1,12 @@
+%% you would expect none of these loops to run...
+% guess which one does
+
+for t = []          %// Iterate an empty 0x0 matrix
+    fprintf('1\n');
+end
+for t = ones(1, 0)  %// Iterate an empty 1x0 matrix
+    fprintf('2\n');
+end
+for t = ones(0, 1)  %// Iterate an empty 0x1 matrix
+    fprintf('3\n');
+end
